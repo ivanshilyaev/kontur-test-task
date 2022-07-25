b833ebec30d8bd5d5aa3e7c46496deba4c1ecaae
Ivan Shilyaev <shilyaev1433@gmail.com>
2022-07-25 10:01:44 +0300

    Initial commit

---
+++ src/main/java/com/ivanshilyaev/kontur/KonturTestTaskApplication.java
@@ -0,0 +1,13 @@
+package com.ivanshilyaev.kontur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KonturTestTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KonturTestTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ivanshilyaev/kontur/KonturTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivanshilyaev.kontur;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KonturTestTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
